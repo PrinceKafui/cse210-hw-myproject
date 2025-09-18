08f2d018950f8e241cf5f258d7d7771b637ee9f5
PrinceKafui <amexoprince@gmail.com>
2025-09-18 16:11:12 +0000

    scripture memorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,38 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        
+        
+        Reference reference = new Reference("Isaiah", 41, 10, 11);
+        string text = " So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand.All who rage against you will surely be ashamed and disgraced; those who oppose you will be as nothing and perish. ";
+        Scripture scripture = new Scripture(reference, text);
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress ENTER to hide words or type 'quit' to exit.");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+
+            if (scripture.AllWordsHidden())
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetDisplayText());
+                Console.WriteLine("\nAll words hidden. Program ending...");
+                break;
+            }
+        }
     }
 }
